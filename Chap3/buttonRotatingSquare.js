87483e766f2a7004755c81144c36478b266b5ed7
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2022-06-26 01:03:00 -0400

    Minor tweaks

--- Chap3/buttonRotatingSquare.js
+++ Chap3/buttonRotatingSquare.js
@@ -1,68 +1,56 @@
 "use strict";
 
-var canvas;
-var gl;
+let canvas, gl, thetaLoc;
+let theta = 0.0;
+let increment = 0.1;
+let shouldRotate = true;
 
-var theta = 0.0;
-var thetaLoc;
-var increment = 0.1;
+window.onload = function () {
+  document.getElementById("toggle").onclick = () => {
+    shouldRotate = !shouldRotate;
+  };
 
-window.onload = function init()
-{
+  document.getElementById("change-direction").onclick = () => {
+    increment = -increment;
+  };
 
-    document.getElementById("toggle").onclick = function( event ) {
-        increment = (increment > 0) ? 0 : 0.1;
-    };
+  canvas = document.getElementById("gl-canvas");
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
 
-    canvas = document.getElementById( "gl-canvas" );
+  //  Configure WebGL
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+  //  Load shaders and initialize attribute buffers
+  let program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
 
-    //
-    //  Configure WebGL
-    //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+  // Load the data into the GPU
+  const vertices = [vec2(0, 1), vec2(-1, 0), vec2(1, 0), vec2(0, -1)];
+  const bufferId = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
-    //  Load shaders and initialize attribute buffers
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+  // Associate out shader letiables with our data buffer
+  let vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
 
-    var vertices = [
-        vec2(  0,  1 ),
-        vec2(  -1,  0 ),
-        vec2( 1,  0 ),
-        vec2(  0, -1 )
-    ];
+  thetaLoc = gl.getUniformLocation(program, "theta");
 
-
-    // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    thetaLoc = gl.getUniformLocation( program, "theta" );
-
-    render();
+  render();
 };
 
-
-
-
 function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.uniform1f(thetaLoc, theta);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-    gl.clear( gl.COLOR_BUFFER_BIT );
-
+  if (shouldRotate) {
     theta += increment;
-    gl.uniform1f( thetaLoc, theta );
-
-    gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
-
-    window.requestAnimFrame(render);
+  }
+  window.requestAnimFrame(render);
 }
