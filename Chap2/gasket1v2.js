5a6387a5d17862c43f4c24c76b3f50d38da9d8ca
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2022-06-26 00:34:39 -0400

    Light cleanup

--- Chap2/gasket1v2.js
+++ Chap2/gasket1v2.js
@@ -1,79 +1,66 @@
 "use strict";
 
-var gl;
-var points;
+let gl, points;
+const NumPoints = 5000;
 
-var NumPoints = 5000;
+window.onload = () => {
+  const canvas = document.getElementById("gl-canvas");
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
 
-window.onload = function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
+  //  Initialize our data for the Sierpinski Gasket
+  // First, initialize the corners of our gasket with three points.
+  const vertices = [vec2(-1, -1), vec2(0, 1), vec2(1, -1)];
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+  // Next, generate the rest of the points, by first finding a random point
+  //  within our gasket boundary.  We use Barycentric coordinates
+  //  (simply the weighted average of the corners) to find the point
+  let coeffs = vec3(Math.random(), Math.random(), Math.random());
+  coeffs = normalize(coeffs);
 
-    //
-    //  Initialize our data for the Sierpinski Gasket
-    //
+  const a = scale(coeffs[0], vertices[0]);
+  const b = scale(coeffs[1], vertices[1]);
+  const c = scale(coeffs[2], vertices[2]);
+  let p = add(a, add(b, c));
 
-    // First, initialize the corners of our gasket with three points.
-    var vertices = [
-        vec2( -1, -1 ),
-        vec2(  0,  1 ),
-        vec2(  1, -1 )
-    ];
+  // Add our randomly chosen point into our array of points
+  points = [p];
 
-    // Next, generate the rest of the points, by first finding a random point
-    //  within our gasket boundary.  We use Barycentric coordinates
-    //  (simply the weighted average of the corners) to find the point
+  for (var i = 0; points.length < NumPoints; ++i) {
+    const randomVertIdx = Math.floor(Math.random() * 3);
+    p = add(points[i], vertices[randomVertIdx]);
+    p = scale(0.5, p);
+    points.push(p);
+  }
 
-    var coeffs = vec3( Math.random(), Math.random(), Math.random() );
-    coeffs = normalize( coeffs );
+  //  Configure WebGL
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    var a = scale( coeffs[0], vertices[0] );
-    var b = scale( coeffs[1], vertices[1] );
-    var c = scale( coeffs[2], vertices[2] );
+  //  Load shaders and initialize attribute buffers
+  const program = initShaders(
+    gl,
+    "shaders/vshader21.glsl",
+    "shaders/fshader21.glsl"
+  );
+  gl.useProgram(program);
 
-    var p = add( a, add(b, c) );
+  // Load the data into the GPU
+  const bufferId = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    // Add our randomly chosen point into our array of points
-    points = [ p ];
+  // Associate out shader variables with our data buffer
+  const vPos = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPos);
 
-    for ( var i = 0; points.length < NumPoints; ++i ) {
-        var j = Math.floor(Math.random() * 3);
-
-        p = add( points[i], vertices[j] );
-        p = scale( 0.5, p );
-        points.push( p );
-    }
-
-    //
-    //  Configure WebGL
-    //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
-    var program = initShaders( gl, "shaders/vshader21.glsl",
-                               "shaders/fshader21.glsl" );
-    gl.useProgram( program );
-
-    // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-
-    // Associate out shader variables with our data buffer
-    var vPos = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPos, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPos );
-
-    render();
+  render();
 };
 
-
-function render()
-{
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.POINTS, 0, points.length );
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.POINTS, 0, points.length);
 }
--- Common/initShaders.js
+++ Common/initShaders.js
@@ -1,57 +1,60 @@
-//
-//  initShaders.js
-//
-
-function initShaders( gl, vertexShaderId, fragmentShaderId )
-{
-    var vertShdr;
-    var fragShdr;
-
-    var vertElem = document.getElementById( vertexShaderId );
-    if ( !vertElem ) { 
-        alert( "Unable to load vertex shader " + vertexShaderId );
-        return -1;
+/**
+ * Compile and link the vertex and fragment shaders
+ *
+ * @param {*} gl
+ * @param {*} vertexShaderId - id for the vertex shader element
+ * @param {*} fragmentShaderId - id for the vertex shader element
+ * @returns
+ */
+function initShaders(gl, vertexShaderId, fragmentShaderId) {
+  const vertElem = document.getElementById(vertexShaderId);
+  if (!vertElem) {
+    const errMsg = `Unable to load vertex shader ${vertexShaderId}; no element found`;
+    alert(errMsg);
+    throw errMsg;
+  } else {
+    var vertShdr = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShdr, vertElem.text);
+    gl.compileShader(vertShdr);
+    if (!gl.getShaderParameter(vertShdr, gl.COMPILE_STATUS)) {
+      const msg = `Vertex shader failed to compile with error: ${gl.getShaderInfoLog(
+        vertShdr
+      )}`;
+      alert(msg);
+      throw msg;
     }
-    else {
-        vertShdr = gl.createShader( gl.VERTEX_SHADER );
-        gl.shaderSource( vertShdr, vertElem.text );
-        gl.compileShader( vertShdr );
-        if ( !gl.getShaderParameter(vertShdr, gl.COMPILE_STATUS) ) {
-            var msg = "Vertex shader failed to compile.  The error log is:"
-        	+ "<pre>" + gl.getShaderInfoLog( vertShdr ) + "</pre>";
-            alert( msg );
-            return -1;
-        }
-    }
+    console.info("Vertex shader initialized");
+  }
 
-    var fragElem = document.getElementById( fragmentShaderId );
-    if ( !fragElem ) { 
-        alert( "Unable to load vertex shader " + fragmentShaderId );
-        return -1;
+  const fragElem = document.getElementById(fragmentShaderId);
+  if (!fragElem) {
+    alert(`Unable to load fragment shader ${vertexShaderId}; no element found`);
+  } else {
+    var fragShdr = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragShdr, fragElem.text);
+    gl.compileShader(fragShdr);
+    if (!gl.getShaderParameter(fragShdr, gl.COMPILE_STATUS)) {
+      const msg = `Fragment shader failed to compile with error: ${gl.getShaderInfoLog(
+        fragShdr
+      )}`;
+      alert(msg);
+      throw msg;
     }
-    else {
-        fragShdr = gl.createShader( gl.FRAGMENT_SHADER );
-        gl.shaderSource( fragShdr, fragElem.text );
-        gl.compileShader( fragShdr );
-        if ( !gl.getShaderParameter(fragShdr, gl.COMPILE_STATUS) ) {
-            var msg = "Fragment shader failed to compile.  The error log is:"
-        	+ "<pre>" + gl.getShaderInfoLog( fragShdr ) + "</pre>";
-            alert( msg );
-            return -1;
-        }
-    }
+    console.info("Fragment shader initialized");
+  }
 
-    var program = gl.createProgram();
-    gl.attachShader( program, vertShdr );
-    gl.attachShader( program, fragShdr );
-    gl.linkProgram( program );
-    
-    if ( !gl.getProgramParameter(program, gl.LINK_STATUS) ) {
-        var msg = "Shader program failed to link.  The error log is:"
-            + "<pre>" + gl.getProgramInfoLog( program ) + "</pre>";
-        alert( msg );
-        return -1;
-    }
+  const program = gl.createProgram();
+  gl.attachShader(program, vertShdr);
+  gl.attachShader(program, fragShdr);
+  gl.linkProgram(program);
 
-    return program;
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    const msg = `Shader program failed to link with error ${gl.getProgramInfoLog(
+      program
+    )}`;
+    alert(msg);
+    throw msg;
+  }
+
+  return program;
 }
